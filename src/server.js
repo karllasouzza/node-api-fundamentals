ee3c2ed6fdadad8eb45ad9fb6c001d47f95c34bb
Karlla Souzza <karlla.souzza7@gmail.com>
2026-02-16 13:08:32 -0300

    feat(server): implement user management endpoints
    - Add GET /users endpoint to retrieve user list
    - Add POST /users endpoint to create a new user
    - Update server to listen on port 3333

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import http from "node:http";
 
+const USERS = [];
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res
+    .setHeader("Content-Type", "application/json")
+    .end(JSON.stringify(USERS));
+  }
+
+  if (method === "POST" && url === "/users") {
+    USERS.push({ id: 1, name: "John Doe", email: "john.doe@example.com" });
+    return res.end("Create a new user");
+  }
+
   res.end("Hello, World!");
 });
 
-server.listen(3002, () => {
-  console.log("Server is running on http://localhost:3000");
+server.listen(3333, () => {
+  console.log("Server is running on http://localhost:3333");
 });
