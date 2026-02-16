143599edc8cd5f55a746856cd029930f838614b1
Karlla Souzza <karlla.souzza7@gmail.com>
2026-02-16 12:41:54 -0300

    feat(server): add basic HTTP server implementation
    - Create a new server.js file
    - Implement a simple HTTP server that responds with "Hello, World!"
    - Configure server to listen on port 3002

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!");
+});
+
+server.listen(3002, () => {
+  console.log("Server is running on http://localhost:3000");
+});
